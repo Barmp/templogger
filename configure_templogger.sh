578a9404cac072ba0d2069c3e304a9628257bc2e
Greg Malone <Barmp@users.noreply.github.com>
2023-10-18 19:56:41 -0700

    Update configure_templogger.sh
    Improved the displayed output of the script, still needs work.

--- configure_templogger.sh
+++ configure_templogger.sh
@@ -4,25 +4,30 @@
 #
 
 echo "Updating the system prior to installling required dependancies."
-echo "This can take some time." 
-
-apt-get update
+echo "This can take some time."
+echo ""
+echo "..."
+apt-get update -u -q
 echo "APT-GET Update completed!"
-
-apt-get upgrade
+echo ""
+echo "..."
+apt-get upgrade -u -q
 echo "System update completed!"
-
-apt-get install -y i2c-tools python3 python3-pip libgpiod-dev 
+echo ""
+echo "..."
+apt-get install -u -q i2c-tools python3 python3-pip libgpiod-dev
 echo "System packages installed!"
-
-pip3 install -r requirements.txt
+echo ""
+echo "..."
+pip3 install -r requirements.txt -U
 echo "Python libraries installed!"
-
-echo " "
-echo " "
-
+echo ""
+echo "..."
+echo "installing as a system service"
 cp /opt/templogger/temperature-probe.service /etc/systemd/system/.
 cp /opt/templogger/temperature-probe.timer /etc/systemd/system/.
 
 systemctl daemon-reload
 systemctl start temperature-probe.service
+echo ""
+echo "Completed!"
